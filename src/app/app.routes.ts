af8406ba919a6d4eee2a524889dbe988e1622ed8
Orestil, Dudley <da191212@ens.uqam.ca>
2026-06-27 14:52:30 -0400

    feat: defining the routes of the application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AboutComponent } from './features/about/about.component';
+import { HomeComponent } from './features/home/home.component';
+import { PortfolioComponent } from './features/portfolio/portfolio.component';
+import { SkillsMatrixComponent } from './features/skills-matrix/skills-matrix.component';
+
+
+
+
+export const routes: Routes = [
+    { 
+        path: '',
+        component: HomeComponent,
+        title: 'Dudley Orestil | Portfolio',
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
+        title: 'About | Dudley Orestil',
+    },
+    {
+        path: 'skills',
+        component: SkillsMatrixComponent,
+        title: 'Skills | Dudley Orestil',
+    },
+    {
+        path: 'portfolio',
+        component: PortfolioComponent,
+        title: 'Portfolio | Dudley Orestil',
+    },
+    {
+        path: '**',
+        redirectTo: '',
+    },
+];
